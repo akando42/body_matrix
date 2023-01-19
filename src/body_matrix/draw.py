c629442f4b019fc5cbb1c787d77d3c3a01bb2b17
Mike Do Consulter <hoang.do.2102@gmail.com>
2023-01-19 13:34:07 +0700

    draw floating rectangle labels

--- src/body_matrix/draw.py
+++ src/body_matrix/draw.py
@@ -102,6 +102,74 @@ def connecting_line(image, pointA, pointB, line_color, line_width):
 
 	return sample
 
-def floating_rectangle_label():
-	return
+def floating_rectangle_label(image, longitude_coordinate, point, label_text, label_size, label_color, label_font, background_color):
+	sample = image.copy()
+	draw = ImageDraw.Draw(sample)
+	radius = 6
+	
+	### Draw a Dot at anchor point
+	draw.ellipse(
+		[(point[0]-radius, point[1]-radius), (point[0]+radius, point[1]+radius)],
+		fill=label_color,
+		outline="#ffffff",
+		width=1
+	)
+
+	### Draw a Label 60 degree North East or North West basing on anchor location
+	marker_font = ImageFont.truetype(label_font, label_size)
+	letter_count = len(label_text)
+	label_height = label_size * 2
+	label_width = label_size * letter_count
+
+	if point[0] < longitude_coordinate:
+		anchor_point = [point[0] - 100, point[1] - 100]
+	else:
+		anchor_point = [point[0] + 100, point[1] - 100]
+
+	rect_topleft = (
+		(anchor_point[0] - label_width/2), 
+		(anchor_point[1] - label_height/2)
+	)
+
+	rect_bottomright = (
+		(anchor_point[0] + label_width/2), 
+		(anchor_point[1] + label_height/2)
+	)
+
+	rect_bottomleft = (
+		rect_topleft[0], 
+		rect_bottomright[1]
+	)
+
+	rect_topright = (
+		rect_topleft[1],
+		rect_bottomright[0]
+	)
+
+	draw.rectangle(
+		[rect_topleft, rect_bottomright], 
+		fill=background_color, 
+		outline=label_color, 
+		width=1
+	)
+
+	draw.text(
+		(anchor_point[0] - label_width/4, anchor_point[1]-label_height/4),
+		str(label_text),
+		fill=label_color,
+		font=marker_font,
+		align="ms"
+	)
+
+	### Draw a Line connecting Anchor Dot and the Label
+	line_color = label_color
+	line_width = int(radius/3)
+
+	draw.line(
+		[point[0], point[1],  anchor_point[0], (anchor_point[1]+label_height/2)], 
+		fill=line_color, 
+		width=line_width
+	)
+
+	return sample 
 
